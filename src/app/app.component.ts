bbdb0c79efc3890666080e207d72323457580f35
_teebo <thaboamrbose.ngubane@absa.africa>
2021-09-10 15:49:55 +0200

    Basic example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'change-detection';
+  _time: any = null;
+
+  constructor(zone: NgZone){
+    this._time = Date.now();
+
+  //  setInterval( // PROBLEM: Runs change detection all the time (miLlisecond change)
+    // Because zone.js causes a change detection for any async event happening in the browser; in this case
+    // the NgZone triggers many change detections because of the setInterval callback which within it ---
+    // the _time is changed thus the getter time which is a constituent of this view's binding (time & textContent of the span element)
+    //   () => {
+    //     this._time = Date.now();
+    //   }
+    // );
+
+    zone.runOutsideAngular(// Running the setIntervalout side the Angular zone so that
+      // the next time change detection happens the value of the getter time will be the synced/uptodate
+
+
+      // Using NgZone to run some code outside of Angular to avoid triggering
+      // change detection is a common optimization technique
+
+
+      // The click event will trigger change detection; because of zone.js - it considers DOM events too; I guess
+      () => {
+        setInterval(
+          () => {
+            this._time = Date.now();
+          }
+        );
+      }
+    )
+  }
+
+  get time() {
+    return this._time;
+  }
 }
